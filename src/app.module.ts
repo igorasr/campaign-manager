200578ba5679c0458098e9c36b9924e9c9347eee
igorasr <igoraugustosouza00@gmail.com>
2025-03-02 10:13:41 -0300

    feat: Configurando modulo principal da aplicacao

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { CampaignsModule } from './campaigns/campaigns.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true, // Apenas para desenvolvimento
+    }),
+    CampaignsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
