59f69787c4577d7230a765fb71976735683eb830
igorasr <igoraugustosouza00@gmail.com>
2025-03-02 11:06:28 -0300

    feat: Removendo codigo desnecessario - configurando eslint para nao adicionar virgula nas linhas - adiciona dependencia faltante

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { CampaignsModule } from './campaigns/campaigns.module';
@@ -12,11 +10,11 @@ import { CampaignsModule } from './campaigns/campaigns.module';
       type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
-      synchronize: true, // Apenas para desenvolvimento
+      synchronize: true // Apenas para desenvolvimento
     }),
-    CampaignsModule,
+    CampaignsModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
